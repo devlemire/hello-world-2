245d493fc0813d02ec36974541ee17f21bfbed2e
= <webdev.jameslemire@gmail.com>
2017-08-15 12:51:33 -0400

    JL master - Fixes.

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.use( cors() );
 app.use( express.static(`${__dirname}/public/build`) );
 
 app.get('/api/test', ( req, res, next ) => {
-  console.log('API 2 HIT');
-  res.status(200).send('API 2 HIT');
+  res.status(200).send('Secret Message 2');
 });
 
 app.listen( config.port || 3000, () => { console.log(`Server listening on port ${ config.port || 3000 }`) } );
