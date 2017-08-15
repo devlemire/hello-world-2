1c09a6dad4109d08ec8e4605fc2d168f6530a00a
= <webdev.jameslemire@gmail.com>
2017-08-15 12:33:01 -0400

    JL master - Move server files.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const config = require('./config');
 const app = express();
 
 app.use( cors() );
-app.use( express.static(`${__dirname}/../public/build`) );
+app.use( express.static(`${__dirname}/public/build`) );
 
 app.get('/api/test', ( req, res, next ) => {
   console.log('API 2 HIT');
